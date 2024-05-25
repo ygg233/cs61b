f5a726d9b06f4efa5929298ad8e73c93d14d18b8
Yang <mc25111@um.edu.mo>
2024-05-25 21:55:29 +0800

    Timing the construction of an AList with a bad resize strategy.

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -23,5 +24,31 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> time = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        Ns.addLast(1000);
+        Ns.addLast(2000);
+        Ns.addLast(4000);
+        Ns.addLast(8000);
+        Ns.addLast(16000);
+        Ns.addLast(32000);
+        Ns.addLast(64000);
+        Ns.addLast(128000);
+
+        for (int i = 0; i < Ns.size(); i++) {
+            long start = System.currentTimeMillis();
+            int opCount = 0;
+            AList<Integer> testList = new AList<>();
+            for (int sz = testList.size(); sz < Ns.get(i); sz++){
+                testList.addLast(sz);
+                opCount++;
+            }
+            long now = System.currentTimeMillis();
+            time.addLast((double)(now - start) / 1000.0D);
+            opCounts.addLast(opCount);
+        }
+        printTimingTable(Ns, time, opCounts);
     }
 }
